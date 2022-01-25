6ced4035050150f7527dfbe547edaeeab0f2f45c
Dida En <den.elfsuju@gmail.com>
2022-01-26 06:22:30 +0700

    Karena sudah benar saat diconsole.log, console.log-nya dihapus.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Sebenarnya jQuery punya method untuk Ajax, yaitu $.ajax. Tapi kalo khusus untuk JSON, bisa pake $.getJSON
 $.getJSON('data/pizza.json', function (data) {
     let menu = data.menu;
-    console.log(menu);
+
 });
