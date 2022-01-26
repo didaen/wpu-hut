e8695a2d5c385cea1cede731fadfe3e383b283a0
Dida En <den.elfsuju@gmail.com>
2022-01-26 15:40:29 +0700

    Meminta jQuery untuk mencari semua elemen dengan class nav-link, ketika diklik menjalankan fungsi, yang pertama menghilangkan semua class active pada elemen dengan nama nav-link. Yang kedua, khusus kepada elemen 'this' yang diklik, tambahkan slass active. Class active ini untuk menunjukkan class yang sedang aktif atau setelah selesia diklik.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ $.getJSON('data/pizza.json', function (data) {
         $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.gambar + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>');
     });
 });
+
+
+// jQuery tolong carikan semua class dengan nama nav-link, kemudian ketika di klik jalankan function berikut.
+$('.nav-link').on('click', function () {
+
+    // Ketika saya klik nav-link yang manapun, saya akan ubah semua nav-link selain yang diklik menjadi tidak punya class active
+    $('.nav-link').removeClass('active');
+
+    // Khusus yang diklik aja, akan punya class active
+    $(this).addClass('active');
+});
