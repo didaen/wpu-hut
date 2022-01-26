7aab0272837c2c3c666efdf9d47b34102c64327f
Dida En <den.elfsuju@gmail.com>
2022-01-26 14:50:18 +0700

    Mencopy semua kode bagian card mulai dari yang class col-md-4 dari file latihan1.php untuk keperluan perulangan dengan JavaScript.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ $.getJSON('data/pizza.json', function (data) {
 
         // jQuery tolong carikan elemen dengan id #daftar-menu kemudian append (tambahkan node baru di posisi paling bawah) code html berikut
 
-        $.('#daftar-menu').append('')
+        $.('#daftar-menu').append('<div class="col-md-4">
+            < div class= "card mb-3" >
+            <img src="img/menu/<?= $row[$i][" gambar"]; ?>" class= "card-img-top" alt = "<?= $row[$i]["nama"]; ?> picture" >
+        <div class="card-body">
+            <h5 class="card-title"><?= $row[$i]["nama"]; ?></h5>
+            <p class="card-text"><?= $row[$i]["deskripsi"]; ?></p>
+            <h5 class="card-title"><?= $row[$i]["harga"]; ?></h5>
+            <a href="#" class="btn btn-primary">Tambahkan</a>
+        </div>
+        </div >
+    </div > ')
     })
 });
