847e12fe2a01d484144313b1a4605657fb1eb862
Dida En <den.elfsuju@gmail.com>
2022-01-31 00:17:02 +0700

    Memperbaiki pengambilan data untuk title-nya, harusnya diambil dari data.nama.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $.getJSON('data/pizza.json', function (data) {
 
         // jQuery tolong carikan elemen dengan id #daftar-menu kemudian append (tambahkan node baru di posisi paling bawah) code html berikut
 
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.gambar + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>');
+        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>');
     });
 });
 
@@ -38,7 +38,7 @@ $.getJSON('data/pizza.json', function (data) {
     $.each(menu, function (i, data) {
         // Jika data.kategori sama dengan isi dari variabel kategori, dipaksa untuk jadi lower case semua
         if (data.kategori == kategori.toLowerCase()) {
-            content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.gambar + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>';
+            content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>';
         }
     });
 });
