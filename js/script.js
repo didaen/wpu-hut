5a69bf2a072a0d6cc04501bbe5af77c6ec6b8aac
Dida En <den.elfsuju@gmail.com>
2022-01-31 00:27:47 +0700

    jQuery tolong carikan saya elemen dengan id daftar menu, kemudian timpa isinya dengan isi dari variabel content.

--- js/script.js
+++ js/script.js
@@ -41,4 +41,7 @@ $.getJSON('data/pizza.json', function (data) {
             content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>';
         }
     });
+
+    // jQuery, tolong carikan saya elemen yang mempunya id daftar-menu lalu timpa apapun isinya dengan content
+    $('#daftar-menu').html(content);
 });
