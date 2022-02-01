e91a04a296a1e2e23019ab8bf0ec1c5244e23980
Dida En <den.elfsuju@gmail.com>
2022-02-01 12:45:37 +0700

    Memindahkan code ke dalam function tampilkanSemuaMenu() untuk membuat function menampilkan semua menu.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 // Untuk mengatasi permasalahan 'All Menu', kita akan membuatnya menjadi function untuk menghindari terlalu banyak pengulangan
 function tampilkanSemuaMenu() {
+    // Sebenarnya jQuery punya method untuk Ajax, yaitu $.ajax. Tapi kalo khusus untuk JSON, bisa pake $.getJSON
+    $.getJSON('data/pizza.json', function (data) {
+        let menu = data.menu;
 
+        // Pengulangan pada jQuery menggunakan $.each
+        $.each(menu, function (i, data) {
+            // i pada parameter adalah index-nya sedangkan data pada parameter adalah data-nya.
+
+            // jQuery tolong carikan elemen dengan id #daftar-menu kemudian append (tambahkan node baru di posisi paling bawah) code html berikut
+
+            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>');
+        });
+    });
 }
 
-// Sebenarnya jQuery punya method untuk Ajax, yaitu $.ajax. Tapi kalo khusus untuk JSON, bisa pake $.getJSON
-$.getJSON('data/pizza.json', function (data) {
-    let menu = data.menu;
-
-    // Pengulangan pada jQuery menggunakan $.each
-    $.each(menu, function (i, data) {
-        // i pada parameter adalah index-nya sedangkan data pada parameter adalah data-nya.
-
-        // jQuery tolong carikan elemen dengan id #daftar-menu kemudian append (tambahkan node baru di posisi paling bawah) code html berikut
-
-        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top" alt ="' + data.nama + ' picture"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '<p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Tambahkan</a></div></div></div>');
-    });
-});
 
 
 // jQuery tolong carikan semua class dengan nama nav-link, kemudian ketika di klik jalankan function berikut.
